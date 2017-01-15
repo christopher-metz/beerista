4bfeb3eb1d1d6bdd65ecb4144d65160b7e9720a6
Jamieson Bates <jamiesonbates@gmail.com>
2017-01-14 16:27:43 -0800

    Tinkered with search page

--- js/search.js
+++ js/search.js
@@ -1,11 +1,7 @@
-const results = document.querySelectorAll('.result');
+const $results = $('.result');
 
 const openBeerPage = function() {
-  $('#search').addClass('off');
-  $('#results').addClass('off');
   $('#beer').removeClass('off');
 }
 
-results.forEach((result) => {
-  result.addEventListener('click', openBeerPage);
-});
+$results.on('click', openBeerPage);
