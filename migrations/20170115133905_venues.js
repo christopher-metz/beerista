b830bcaaae9c0ed30b1deda2b907a6ee327523b9
Christopher Metz <cmetz7@gmail.com>
2017-01-15 13:39:47 -0800

    Added venues migration file

---
+++ migrations/20170115133905_venues.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('venues', (table) => {
+    table.increments();
+    table.string('name').notNullable().defaultTo('');
+    table.string('city').notNullable().defaultTo('');
+    table.string('state').notNullable().defaultTo('');
+    table.text('description').notNullable().defaultTo('');
+    table.string('website').notNullable().defaultTo('');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('venues');
+};
