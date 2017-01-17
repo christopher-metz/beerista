3127883e8485d77cdd196893f76f6e76a9707081
Christopher Metz <cmetz7@gmail.com>
2017-01-16 18:04:08 -0800

    Tinkered with profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,1 +1,12 @@
-'use strict';
+(function() {
+  'use strict';
+
+  $.getJSON('/ratings')
+    .done((ratings) => {
+      // fill out profile page
+      }
+    })
+    .fail(() => {
+      window.location.href = '/login.html';
+    });
+})();
