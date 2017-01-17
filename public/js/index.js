7030ce5e3a7c1f77a14a28757f974f816716a1e6
Jamieson Bates <jamiesonbates@gmail.com>
2017-01-17 11:39:39 -0800

    Add authorization to index and search pages, Add account icon and menu dropdown to index and search pages, Add routing to different pages via account dropdown menu

---
+++ public/js/index.js
@@ -0,0 +1,44 @@
+(function() {
+  'use strict';
+
+  // Check and verify logged in user
+  const checkForCookie = function() {
+    const $xhr = $.ajax({
+      method: 'GET',
+      url: '/token',
+      dataType: 'JSON'
+    })
+    .done((bool) => {
+      if ($xhr.status !== 200) {
+        return;
+      }
+
+      if (bool) {
+        $('#login').addClass('off');
+        $('#account-icon').removeClass('off');
+      }
+    })
+    .fail((err) => {
+      console.log(err);
+    })
+  }
+
+  $(window).on('load', checkForCookie);
+
+  // Toggle Account Menu
+  const toggleAccountMenu = function() {
+    $('#drop-down-container').toggleClass('off');
+  }
+
+  $('#account-icon').on('click', toggleAccountMenu);
+
+  // Navigate to other pages via Account Menu
+  // const myFriends = $('#my-friends');
+  const myBeers = $('#my-beers');
+  // const settings = $('#settings');
+
+  // myFriends.on('click', function() { window.location.href = '/friends.html' });
+  myBeers.on('click', function() { window.location.href = '/profile.html' });
+
+  // const menuOptions = $('#log-out');
+})();
